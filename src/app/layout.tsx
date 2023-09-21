f69feb9ccd37d7d0fcc3dab2f6ec7ac714e1d1e9
bidanee <dleksql119@gmail.com>
2023-09-21 14:12:54 +0900

    파비콘, 타이틀 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,14 @@
+import Header from "./components/Header";
 import "./globals.css";
+
+export const metadata = {
+  title: "[FE] 이단비",
+  description: "이단비의 포트폴리오",
+  keywords: ["포트폴리오", "프론트엔드 개발자"],
+  icons: {
+    icon: "icon.png",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -7,6 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html>
+      <link
+        rel="icon"
+        href="/icon?<generated>"
+        type="image/<generated>"
+        sizes="<generated>"
+      />
       <body>{children}</body>
     </html>
   );
