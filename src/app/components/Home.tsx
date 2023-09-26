6d211b61e50ab65cfd6a9a9502e44d165873aa7d
bidanee <dleksql119@gmail.com>
2023-09-26 11:34:44 +0900

    home, projects파일 추가

---
+++ src/app/components/Home.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div id="home">
+      <div className="mt-[4.5rem]">
+        <div className="flex flex-col">
+          <h1>안녕하세요. 프론트엔드 개발자 이단비입니다.</h1>
+          <p className="flex flex-col">
+            <span></span>
+            <span>프론트엔드 개발자 이단비입니다.</span>
+            <span>프론트엔드 개발자 이단비입니다.</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/components/Projects.tsx
@@ -0,0 +1,7 @@
+export default function Projects() {
+  return (
+    <div id="projects">
+      <div className="mt-[4.5rem]">Projects</div>
+    </div>
+  );
+}
